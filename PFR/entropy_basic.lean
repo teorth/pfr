af93db553877993d45d059a43460054a7db8377b
teorth <tao@math.ucla.edu>
2023-11-12 19:27:46 -0800

    Proved the Jensen bound

--- PFR/entropy_basic.lean
+++ PFR/entropy_basic.lean
@@ -160,5 +160,38 @@ lemma entropy_nonneg [ProbSpace Ω] [Fintype S] (X : Ω → S) : 0 ≤ H[ X ] :=
 /-- The Jensen bound --/
 lemma entropy_le_log [ProbSpace Ω] [Fintype S] {X : Ω → S} (hX : Measurable X): H[ X ] ≤ log (Fintype.card S) := by
   set N := Fintype.card S
-  have : 0 < N := range_nonempty' S
-  sorry
+  have : 0 < N := range_nonempty' hX
+  unfold entropy
+  have hN : log N = N * h (∑ s : S, N⁻¹ * P[ X ⁻¹' {s} ]) := by
+    rw [<-Finset.mul_sum]
+    norm_cast
+    rw [totalProb hX]
+    simp
+    unfold h
+    rw [log_inv]
+    field_simp; ring
+  rw [hN, <- inv_mul_le_iff, Finset.mul_sum]
+  set w := fun _ : S ↦ N⁻¹
+  set p := fun s : S ↦ (P[ X ⁻¹' {s} ] : ℝ)
+
+  conv =>
+    congr
+    . congr; rfl
+      ext s
+      rw [(show N⁻¹ = w s by simp), (show P[ X ⁻¹' {s} ] = p s by simp)]
+    congr; congr; rfl
+    ext s
+    rw [(show N⁻¹ = w s by simp), (show P[ X ⁻¹' {s} ] = p s by simp)]
+  have hf := h_concave
+  have h0 : ∀ s ∈ Finset.univ, 0 ≤ w s := by intros; simp
+  have h1 : ∑ s in Finset.univ, w s = 1 := by
+    simp
+    apply mul_inv_cancel
+    positivity
+  have hmem : ∀ s ∈ Finset.univ, p s ∈ (Set.Icc 0 1) := by
+    intro s _
+    simp
+    norm_cast
+    exact prob_le_one (X ⁻¹' {s})
+  convert (ConcaveOn.le_map_sum hf h0 h1 hmem)
+  positivity
