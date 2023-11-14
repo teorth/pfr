fa1ab611fd2038bdfab74f322c3b98bdac46129b
Scott Morrison <scott.morrison@gmail.com>
2023-11-14 17:42:55 +1100

    recommended lakefile contents

--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,33 @@
 import Lake
 open Lake DSL
 
-package «pfr» {
+def moreServerArgs := #[
+  "-Dpp.unicode.fun=true", -- pretty-prints `fun a ↦ b`
+  "-Dpp.proofs.withType=false"
+]
+
+-- These settings only apply during `lake build`, but not in VSCode editor.
+def moreLeanArgs := moreServerArgs
+
+-- These are additional settings which do not affect the lake hash,
+-- so they can be enabled in CI and disabled locally or vice versa.
+-- Warning: Do not put any options here that actually change the olean files,
+-- or inconsistent behavior may result
+def weakLeanArgs : Array String :=
+  if get_config? CI |>.isSome then
+    #["-DwarningAsError=true"]
+  else
+    #[]
+
+package «PFR» where
+  moreServerArgs := moreServerArgs
   -- add any package configuration options here
-}
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «Pfr» {
+lean_lib «PFR» where
+  moreLeanArgs := moreLeanArgs
+  weakLeanArgs := weakLeanArgs
   -- add any library configuration options here
-}
