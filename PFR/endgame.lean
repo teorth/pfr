85f77106b3f388041ddae5a52f3631cf0eb16c44
teorth <tao@math.ucla.edu>
2023-11-16 10:46:24 -0800

    some updates to entropy blueprint

--- PFR/endgame.lean
+++ PFR/endgame.lean
@@ -19,6 +19,20 @@ Assumptions (need to be placed explicitly in the file):
 * S := X_1 + X_2 + \tilde X_1 + \tilde X_2.
 -/
 
-/-- I(U : V | S) + I(V : W | S) + I(W : U | S) \leq 6 \eta k - \frac{1 - 5 \eta}{1-\eta} (2 \eta k - I_1).
+/--
+$$ I(U : V | S) + I(V : W | S) + I(W : U | S) $$
+is less than or equal to
+$$ 6 \eta k - \frac{1 - 5 \eta}{1-\eta} (2 \eta k - I_1).$$
 -/
 lemma sum_condMutual_le : 0 = 1 := by sorry
+
+/--
+$$ \sum_{i=1}^2 \sum_{A\in\{U,V,W\}} \big(d[X^0_i;A|S] - d[X^0_i;X_i]\big)$$
+is less than or equal to
+$$ \leq (6 - 3\eta) k + 3(2 \eta k - I_1).$$
+-/
+lemma sum_dist_diff_le : 0 = 1 := by sorry
+
+/-- $U+V+W=0$. -/
+lemma sum_uvw_eq_zero : 0 = 1 := by sorry
+
