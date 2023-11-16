c67434e83a6a9a8a2db958ec2bce3ef843a2efed
teorth <tao@math.ucla.edu>
2023-11-15 17:46:31 -0800

    added some trivial lemmas to f2_vec

--- PFR/f2_vec.lean
+++ PFR/f2_vec.lean
@@ -17,4 +17,10 @@ open Pointwise Nat
 
 class ElementaryAddGroup (G : Type*) [AddGroup G] (p : outParam ℕ) where
   orderOf_of_ne {x : G} (hx : x ≠ 0) : addOrderOf x = p
--- may want to change this to p . x = 0 for all x; may also want to enforce finiteness.
+-- may want to change this to p . x = 0 for all x
+
+variable [AddGroup G] [ElementaryAddGroup G 2]
+
+lemma sum_eq_diff ( x y : G ) : x + y = x - y := by sorry
+
+lemma sum_add_sum_eq_sum ( x y z : G ) : (x + y) + (y + z) = x + z := by sorry
