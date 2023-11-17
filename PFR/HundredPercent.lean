ae927f04efeb640ff5a10686734977f707e61cd8
teorth <tao@math.ucla.edu>
2023-11-17 13:47:13 -0800

    split up 100% proof

--- PFR/HundredPercent.lean
+++ PFR/HundredPercent.lean
@@ -4,5 +4,8 @@
 Here we show entropic PFR in the case of doubling constant zero.
 -/
 
-/-- If $d[X_1;X_2]=0$, then there exists a subgroup $H \leq G$ such that $d[X_1;U_H] = d[X_2;U_H] = 0$. -/
+/-- If $d[X_1;X_1]=0$, then there exists a subgroup $H \leq G$ such that $d[X_1;U_H] = 0$. -/
+theorem dist_self__eq_zero_iff : 0 = 1 := by sorry
+
+/-- If $d[X_1;X_2]=0$, then there exists a subgroup $H \leq G$ such that $d[X_1;U_H] = d[X_2;U_H] = 0$. Follows from the preceding claim by the triangle inequality -/
 theorem dist_eq_zero_iff : 0 = 1 := by sorry
