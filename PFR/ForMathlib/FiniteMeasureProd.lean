254051875d875ed0ddba37e8a3c2026d7e515c00
Kalle <kalle.kytola@aalto.fi>
2023-11-23 22:55:48 +0200

    Basic API for products of probability measures.

--- PFR/ForMathlib/FiniteMeasureProd.lean
+++ PFR/ForMathlib/FiniteMeasureProd.lean
@@ -18,14 +18,15 @@ namespace MeasureTheory
     (μ : FiniteMeasure α) (f : α → β) :
     (μ.map f).toMeasure = μ.toMeasure.map f := rfl
 
+@[simp] lemma ProbabilityMeasure.toMeasure_map {α β : Type*} [MeasurableSpace α] [MeasurableSpace β]
+    (μ : ProbabilityMeasure α) {f : α → β} (hf : AEMeasurable f μ) :
+    (μ.map hf).toMeasure = μ.toMeasure.map f := rfl
+
 section FiniteMeasure_product
 
 namespace FiniteMeasure
 
-variable {α : Type*} [MeasurableSpace α]
--- [TopologicalSpace α] [OpensMeasurableSpace α]
-variable {β : Type*} [MeasurableSpace β]
--- [TopologicalSpace β] [OpensMeasurableSpace β]
+variable {α : Type*} [MeasurableSpace α] {β : Type*} [MeasurableSpace β]
 
 /-- The binary product of finite measures. -/
 noncomputable def prod (μ : FiniteMeasure α) (ν : FiniteMeasure β) : FiniteMeasure (α × β) :=
@@ -177,6 +178,102 @@ end FiniteMeasure -- namespace
 end FiniteMeasure_product -- section
 
 section ProbabilityMeasure_product
+
+namespace ProbabilityMeasure
+
+variable {α : Type*} [MeasurableSpace α] {β : Type*} [MeasurableSpace β]
+
+/-- The binary product of probability measures. -/
+noncomputable def prod (μ : ProbabilityMeasure α) (ν : ProbabilityMeasure β) :
+    ProbabilityMeasure (α × β) :=
+  ⟨μ.toMeasure.prod ν.toMeasure, Measure.prod.instIsProbabilityMeasure μ.toMeasure ν.toMeasure⟩
+
+variable (μ : ProbabilityMeasure α) (ν : ProbabilityMeasure β)
+
+@[simp] lemma toMeasure_prod : (μ.prod ν).toMeasure = μ.toMeasure.prod ν.toMeasure := rfl
+
+lemma prod_apply (s : Set (α × β)) (s_mble : MeasurableSet s) :
+    μ.prod ν s = ENNReal.toNNReal (∫⁻ x, ν.toMeasure (Prod.mk x ⁻¹' s) ∂μ) := by
+  simp [@Measure.prod_apply α β _ _ μ ν _ s s_mble]
+
+lemma prod_apply_symm (s : Set (α × β)) (s_mble : MeasurableSet s) :
+    μ.prod ν s = ENNReal.toNNReal (∫⁻ y, μ.toMeasure ((fun x ↦ ⟨x, y⟩) ⁻¹' s) ∂ν) := by
+  simp [@Measure.prod_apply_symm α β _ _ μ ν _ _ s s_mble]
+
+lemma prod_prod (s : Set α) (t : Set β) : μ.prod ν (s ×ˢ t) = μ s * ν t := by simp
+
+example : Measurable (Prod.fst : α × β → α) := by
+  exact measurable_fst
+
+@[simp] lemma map_fst_prod : (μ.prod ν).map measurable_fst.aemeasurable = μ := by
+  apply Subtype.ext
+  simp only [val_eq_to_measure, toMeasure_map, toMeasure_prod, Measure.map_fst_prod,
+             measure_univ, one_smul]
+
+@[simp] lemma map_snd_prod : (μ.prod ν).map measurable_snd.aemeasurable = ν := by
+  apply Subtype.ext
+  simp only [val_eq_to_measure, toMeasure_map, toMeasure_prod, Measure.map_snd_prod,
+             measure_univ, one_smul]
+
+example  {α' : Type*} [MeasurableSpace α'] {β' : Type*} [MeasurableSpace β']
+    {f : α → α'} {g : β → β'} (f_mble : Measurable f) (g_mble : Measurable g) :
+    Measurable (Prod.map f g) := by
+  exact Measurable.prod_map f_mble g_mble
+
+example  {α' : Type*} [MeasurableSpace α'] {β' : Type*} [MeasurableSpace β']
+    {f : α → α'} {g : β → β'} (f_mble : AEMeasurable f μ) (g_mble : AEMeasurable g ν) :
+    AEMeasurable (Prod.map f g) (μ.toMeasure.prod ν.toMeasure) := by
+  --exact?
+  sorry
+
+lemma map_prod_map {α' : Type*} [MeasurableSpace α'] {β' : Type*} [MeasurableSpace β']
+    {f : α → α'} {g : β → β'} (f_mble : Measurable f) (g_mble : Measurable g) :
+    (μ.map f_mble.aemeasurable).prod (ν.map g_mble.aemeasurable)
+      = (μ.prod ν).map (f_mble.prod_map g_mble).aemeasurable := by
+  apply Subtype.ext
+  simp only [val_eq_to_measure, toMeasure_prod, toMeasure_map]
+  rw [Measure.map_prod_map _ _ f_mble g_mble] <;>
+    exact IsFiniteMeasure.toSigmaFinite (Measure.map _ _)
+
+lemma prod_apply_null {s : Set (α × β)} (hs : MeasurableSet s) :
+    μ.prod ν s = 0 ↔ (fun x ↦ ν (Prod.mk x ⁻¹' s)) =ᵐ[μ] 0 := by
+  convert Measure.measure_prod_null (μ := μ.toMeasure) (ν := ν.toMeasure) hs
+  · simp only [toMeasure_prod, toNNReal_eq_zero_iff, or_iff_left_iff_imp]
+    intro con
+    by_contra
+    exact measure_ne_top _ _ con
+  · constructor <;> intro h <;> filter_upwards [h] with p hp
+    · simp only [Pi.zero_apply] at *
+      rcases (ENNReal.toNNReal_eq_zero_iff _).mp hp with (h'|con)
+      · exact h'
+      · by_contra
+        exact measure_ne_top _ _ con
+    · simp only [Pi.zero_apply] at *
+      exact (ENNReal.toNNReal_eq_zero_iff _).mpr (Or.inl hp)
+
+lemma measure_ae_null_of_prod_null {s : Set (α × β)} (h : μ.prod ν s = 0) :
+    (fun x ↦ ν (Prod.mk x ⁻¹' s)) =ᵐ[μ] 0 := by
+  convert Measure.measure_ae_null_of_prod_null (μ := μ.toMeasure) (ν := ν.toMeasure) (s := s) ?_
+  · constructor <;> intro h <;> filter_upwards [h] with p hp
+    · simp only [Pi.zero_apply] at *
+      rcases (ENNReal.toNNReal_eq_zero_iff _).mp hp with (h'|con)
+      · exact h'
+      · by_contra
+        exact measure_ne_top _ _ con
+    · simp only [Pi.zero_apply] at *
+      exact (ENNReal.toNNReal_eq_zero_iff _).mpr (Or.inl hp)
+  · simp [toNNReal_eq_zero_iff] at h
+    rcases h with (h'|con)
+    · exact h'
+    · by_contra
+      exact measure_ne_top _ _ con
+
+lemma prod_swap : (μ.prod ν).map measurable_swap.aemeasurable = ν.prod μ := by
+  apply Subtype.ext
+  simp [Measure.prod_swap]
+
+end ProbabilityMeasure -- namespace
+
 end ProbabilityMeasure_product -- section
 
 end MeasureTheory -- namespace
