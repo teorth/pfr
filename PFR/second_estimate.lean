e50252f26d808360c1fdf4a23b505fbd68c768b9
Ben Eltschig <b.eltschig@protonmail.com>
2023-11-27 23:55:02 +0100

    finish second estimate

--- PFR/second_estimate.lean
+++ PFR/second_estimate.lean
@@ -66,9 +66,14 @@ lemma rdist_of_sums_ge' : d[X₁ + X₁' # X₂ + X₂'] ≥ k - η * (d[X₁ # 
 lemma second_estimate : I₂ ≤ 2 * η * k + (2 * η * (2 * η * k - I₁)) / (1 - η) := by
   have hX₁_indep : IndepFun X₁ X₁' (μ := ℙ) := h_indep.indepFun (show 0 ≠ 2 by decide)
   have hX₂_indep : IndepFun X₂ X₂' (μ := ℙ) := h_indep.indepFun (show 1 ≠ 3 by decide)
+  have hX_indep : IndepFun (X₁ + X₁') (X₂ + X₂') := by
+    exact h_indep.indepFun_add_add (ι := Fin 4) (by intro i; fin_cases i <;> assumption) 0 2 1 3
+      (by decide) (by decide) (by decide) (by decide)
   let Y : Fin 4 → Ω → G := ![X₂, X₁, X₂', X₁']
   have hY : ∀ i, Measurable (Y i) := fun i => by fin_cases i <;> assumption
-  have hY_indep : iIndepFun (fun _ => hG) Y := by sorry
+  have hY_indep : iIndepFun (fun _ => hG) Y := by
+    let σ : Fin 4 ≃ Fin 4 := { toFun := ![1, 0, 3, 2], invFun := ![1, 0, 3, 2], left_inv := by intro i; fin_cases i <;> rfl, right_inv := by intro i; fin_cases i <;> rfl }
+    refine' iIndepFun.reindex σ _; convert h_indep using 1; ext i; fin_cases i <;> rfl
   have h := sum_of_rdist_eq_char_2 Y hY_indep hY
   rw [show Y 0 = X₂ by rfl, show Y 1 = X₁ by rfl, show Y 2 = X₂' by rfl, show Y 3 = X₁' by rfl] at h
   rw [← h₂.rdist_eq h₁, rdist_symm, rdist_symm (X:= X₂ + X₂'),
@@ -83,13 +88,11 @@ lemma second_estimate : I₂ ≤ 2 * η * k + (2 * η * (2 * η * k - I₁)) / (
     rw [← add_comm X₁ X₁']
     have h₁'' := mul_le_mul_of_nonneg_left h₁' (show 0 ≤ η by rw [η]; positivity)
     have h₂'' := mul_le_mul_of_nonneg_left h₂' (show 0 ≤ η by rw [η]; positivity)
-    have : d[X₁ | X₁ + X₁' # X₂ | X₂ + X₂'] ≤ d[X₁ + X₁' # X₂ + X₂'] := by sorry
+    have := rdist_of_sums_ge' p _ _ _ _ hX₁ hX₂ hX₁' hX₂' h₁ h₂ h_indep h_min
     linarith
-  save
   nth_rewrite 1 [mul_div_assoc, ← mul_add, mul_assoc, mul_left_comm]
   refine' h''.trans (mul_le_mul_of_nonneg_left _ (show 0 ≤ η by rw [η]; positivity))
   have h : d[X₁ + X₁' # X₂+ X₂'] ≤ (2 + η) * k - (d[X₁# X₁] + d[X₂ # X₂]) / 2 - I₁ := by
-    have hX_indep : IndepFun (X₁ + X₁') (X₂ + X₂') := by sorry
     have h := hX_indep.rdist_eq (hX₁.add hX₁') (hX₂.add hX₂')
     rw [pi.sub_eq_add (X₁ + X₁') (X₂ + X₂'), ← pi.sub_eq_add X₁ X₁', ← pi.sub_eq_add X₂ X₂',
       sub_eq_iff_eq_add.mp (sub_eq_iff_eq_add.mp (hX₁_indep.rdist_eq hX₁ hX₁').symm),
@@ -98,7 +101,9 @@ lemma second_estimate : I₂ ≤ 2 * η * k + (2 * η * (2 * η * k - I₁)) / (
       ← (IdentDistrib.refl hX₁.aemeasurable).rdist_eq h₁,
       ← (IdentDistrib.refl hX₂.aemeasurable).rdist_eq h₂,
       pi.sub_eq_add X₁ X₁', pi.sub_eq_add X₂ X₂', ← add_assoc, add_right_comm _ X₁'] at h
-    have h_indep' : iIndepFun (fun _i => hG) ![X₁, X₂, X₂', X₁'] := by sorry
+    have h_indep' : iIndepFun (fun _i => hG) ![X₁, X₂, X₂', X₁'] := by
+      let σ : Fin 4 ≃ Fin 4 := { toFun := ![0, 1, 3, 2], invFun := ![0, 1, 3, 2], left_inv := by intro i; fin_cases i <;> rfl, right_inv := by intro i; fin_cases i <;> rfl }
+      refine' iIndepFun.reindex σ.symm _; convert h_indep using 1; ext i; fin_cases i <;> rfl
     have h' := ent_ofsum_le p X₁ X₂ X₁' X₂' hX₁ hX₂ hX₁' hX₂' h₁ h₂ h_indep' h_min
     convert (h.symm ▸ (sub_le_sub_right (sub_le_sub_right h' _) _)) using 1; ring
   have h' := (rdist_of_sums_ge' p X₁ X₂ X₁' X₂' hX₁ hX₂ hX₁' hX₂' h₁ h₂ h_indep h_min).le.trans h
