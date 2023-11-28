fdace3dd2721f85b26423e17131e0ca6b855e5ae
Patrick Massot <patrickmassot@free.fr>
2023-11-28 08:43:58 -0500

    Sets multiplication and morphims lemmas

---
+++ PFR/ForMathlib/MonoidHom.lean
@@ -0,0 +1,23 @@
+import Mathlib.Algebra.Group.Hom.Defs
+import Mathlib.Data.Set.Pointwise.Basic
+
+open Set Pointwise Function
+
+@[to_additive]
+lemma mul_subset_range {F α β : Type*} [Mul α] [Mul β] [MulHomClass F α β]
+    {f : F} {s t : Set β} (hs : s ⊆ range f) (ht : t ⊆ range f) : s * t ⊆ range f := by
+  rintro _ ⟨x, y, hx, hy, rfl⟩
+  rcases hs hx with ⟨z, rfl⟩
+  rcases ht hy with ⟨w, rfl⟩
+  exact ⟨z*w, map_mul _ _ _⟩
+
+@[to_additive]
+lemma Set.preimage_mul_preimage {F α β : Type*} [Mul α] [Mul β] [MulHomClass F α β]
+    {m : F} (hm : Injective m) {s t : Set β} (hs : s ⊆ range m) (ht : t ⊆ range m) :
+    m ⁻¹' s * m ⁻¹' t = m ⁻¹' (s * t) := by
+  apply subset_antisymm (preimage_mul_preimage_subset m)
+  rintro x ⟨y, z, hy, hz, hx : y * z = _⟩
+  rcases hs hy with ⟨u, rfl⟩
+  rcases ht hz with ⟨v, rfl⟩
+  erw [← map_mul] at hx
+  use u, v, hy, hz, hm hx
