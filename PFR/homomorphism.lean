25c2dfe612ebcb8e270cf628f3f56c23f4cfd434
teorth <tao@math.ucla.edu>
2023-11-29 14:05:01 -0500

    formalized lemmas

--- PFR/homomorphism.lean
+++ PFR/homomorphism.lean
@@ -5,17 +5,17 @@ open Pointwise
 variable {G G' : Type*} [AddCommGroup G] [ElementaryAddCommGroup G 2] [Fintype G] [AddCommGroup G'] [ElementaryAddCommGroup G' 2] [Fintype G']
 
 /-- Let $H_0$ be a subgroup of $G$.  Then every homomorphism $\phi: H_0 \to G'$ can be extended to a homomorphism $\tilde \phi: G \to G'$. -/
-lemma hahn_banach : 0 = 1 := sorry
+lemma hahn_banach (H₀ : AddSubgroup G) (φ : H₀ →+ G') : ∃ (φ' : G →+ G'), ∀ x : H₀, φ x = φ' x := sorry
 
 
 /-- Let $H$ be a subgroup of $G \times G'$.  Then there exists a subgroup $H_0$ of $G$, a subgroup $H_1$ of $G'$, and a homomorphism $\phi: G \to G'$ such that
 $$ H := \{ (x, \phi(x) + y): x \in H_0, y \in H_1 \}.$$
 In particular, $|H| = |H_0| |H_1|$. -/
-lemma goursat : 0 = 1 := by sorry
+lemma goursat (H : AddSubgroup (G × G') ): ∃ (H₀ : AddSubgroup G) (H₁ : AddSubgroup G' ) (φ : G →+ G'), (∀ x : G × G', x ∈ H ↔ (x.1 ∈ H₀ ∧ x.2 - φ x.1 ∈ H₁)) ∧ (Nat.card H) = (Nat.card H₀) * (Nat.card H1) := by sorry
 
 
 /-- Let $f: G \to G'$ be a function, and let $S$ denote the set
 $$ S := \{ f(x+y)-f(x)-f(y): x,y \in G \}.$$
 Then there exists a homomorphism $\phi: G \to G'$ such that
 $$ |\{ f(x) - \phi(x)\}| \leq 4 |S|^{24}.$$ -/
-theorem homomorphism_pfr : 0 = 1 := by sorry
+theorem homomorphism_pfr (f : G → G') (S : Set G') (hS: ∀ x y : G, f (x+y) - (f x) - (f y) ∈ S): ∃ (φ : G →+ G') (T : Set G'), Nat.card T ≤ 4 * (Nat.card S)^24 ∧ ∀ x : G, (f x) - (φ x) ∈ T := by sorry
