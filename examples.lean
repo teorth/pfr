223241ba0bedbe57c4af4fd0d012a7082821698e
teorth <tao@math.ucla.edu>
2023-11-29 09:40:50 -0500

    created examples

--- examples.lean
+++ examples.lean
@@ -1,16 +1,17 @@
 import PFR.main
 
 
+open Pointwise
+
+
 
 /-- A self-contained version of the PFR conjecture using only Mathlib definitions. -/
-
-
-example {G : Type*} [AddCommGroup G] [ElementaryAddCommGroup G 2]
+example {G : Type*} [AddCommGroup G] [Module (ZMod 2) G]
     {A : Set G} {K : ℝ} (h₀A : A.Nonempty) (Afin : A.Finite)
     (hA : Nat.card (A + A) ≤ K * Nat.card A) :
     ∃ (H : AddSubgroup G) (c : Set G), c.Finite ∧ (H : Set G).Finite ∧
       Nat.card c < 2 * K ^ 12 ∧ Nat.card H ≤ Nat.card A ∧ A ⊆ c + H := by
-
-
+  convert PFR_conjecture' h₀A Afin hA
+  norm_cast
 
 --- Maybe some examples to demonstrate the notions of Shannon entropy, mutual information, Ruzsa distance, and state some of our inequalities about them (E.g., Ruzsa triangle inequality?)
