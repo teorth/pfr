bfb75fc702022f1e29556406fa6ee688846e35e0
L Lllvvuu <git@llllvvuu.dev>
2023-12-05 21:03:50 -0800

    perf(homomorphism): save ~10 seconds or so
    mostly:
    - ungolfing simp -> rw -> show/exact
    - adding explicit types annotations and constructors
    wherever there's a simple way to do so

--- PFR/Mathlib/Algebra/Module/Submodule/Map.lean
+++ PFR/Mathlib/Algebra/Module/Submodule/Map.lean
@@ -7,8 +7,8 @@ variable { M M' R : Type* } [Semiring R] [AddCommMonoid M] [AddCommMonoid M']
 
 def submoduleMap (f : M →ₗ[R] M') (S : Submodule R M) : S →ₗ[R] S.map f where
   toFun := fun x ↦ ⟨ f x, Submodule.apply_coe_mem_map f x ⟩
-  map_add' := by simp
-  map_smul' := by simp
+  map_add' x y := Subtype.eq <| f.map_add x y
+  map_smul' x y := Subtype.eq <| f.map_smul x y
 
 theorem submoduleMap_surjective (f : M →ₗ[R] M') (S : Submodule R M) :
     Function.Surjective (f.submoduleMap S) :=
