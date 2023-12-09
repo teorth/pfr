5a2fed7f6c0c15cdf0782167dd1f4ff6d4298ef8
teorth <tao@math.ucla.edu>
2023-12-09 14:38:36 -0800

    added improvedPFR stubs

---
+++ PFR/ImprovedPFR.lean
@@ -0,0 +1,139 @@
+import PFR.Fibring
+import PFR.TauFunctional
+
+/-!
+# Improved PFR
+
+An improvement to PFR that lowers the exponent from 12 to 11.
+
+## Main results
+
+*
+-/
+
+section MainEstimates
+
+open MeasureTheory ProbabilityTheory
+
+variable {G : Type*} [addgroup: AddCommGroup G] [Fintype G] [hG : MeasurableSpace G]
+  [MeasurableSingletonClass G] [elem: ElementaryAddCommGroup G 2] [MeasurableAdd₂ G]
+
+variable {Ω₀₁ Ω₀₂ : Type*} [MeasureSpace Ω₀₁] [MeasureSpace Ω₀₂]
+  [IsProbabilityMeasure (ℙ : Measure Ω₀₁)] [IsProbabilityMeasure (ℙ : Measure Ω₀₂)]
+
+variable (p : refPackage Ω₀₁ Ω₀₂ G)
+
+variable {Ω : Type*} [MeasureSpace Ω] [IsProbabilityMeasure (ℙ : Measure Ω)]
+
+variable (X₁ X₂ X₁' X₂' : Ω → G)
+  (hX₁ : Measurable X₁) (hX₂ : Measurable X₂) (hX₁' : Measurable X₁') (hX₂' : Measurable X₂')
+
+variable (h₁ : IdentDistrib X₁ X₁') (h₂ : IdentDistrib X₂ X₂')
+
+variable (h_indep : iIndepFun (fun _i => hG) ![X₁, X₂, X₂', X₁'])
+
+variable (h_min: tau_minimizes p X₁ X₂)
+
+/-- `k := d[X₁ # X₂]`, the Ruzsa distance `rdist` between X₁ and X₂. -/
+local notation3 "k" => d[X₁ # X₂]
+
+/-- `U := X₁ + X₂` -/
+local notation3 "U" => X₁ + X₂
+
+/-- `V := X₁' + X₂` -/
+local notation3 "V" => X₁' + X₂
+
+/-- `W := X₁' + X₁` -/
+local notation3 "W" => X₁' + X₁
+
+/-- `S := X₁ + X₂ + X₁' + X₂'` -/
+local notation3 "S" => X₁ + X₂ + X₁' + X₂'
+
+/-- `I₁ := I[X₁ + X₂ : X₁' + X₂ | X₁ + X₂ + X₁' + X₂']`, the conditional mutual information
+of `X₁ + X₂` and `X₁' + X₂` given the quadruple sum `X₁ + X₂ + X₁' + X₂'`. -/
+local notation3 "I₁" => I[X₁ + X₂ : X₁' + X₂ | X₁ + X₂ + X₁' + X₂']
+
+/-- `I₂ := I[X₁ + X₂ : X₁' + X₁ | X₁ + X₂ + X₁' + X₂']`, the conditional mutual information
+of `X₁ + X₂` and `X₁' + X₁` given the quadruple sum `X₁ + X₂ + X₁' + X₂'`. -/
+local notation3 "I₂" => I[X₁ + X₂ : X₁' + X₁ | X₁ + X₂ + X₁' + X₂']
+
+variable {Ω' : Type*} [MeasureSpace Ω'] [IsProbabilityMeasure (ℙ : Measure Ω')]
+variable {T₁ T₂ T₃ : Ω' → G} (hT : T₁+T₂+T₃ = 0)
+variable (hT₁ : Measurable T₁) (hT₂ : Measurable T₂) (hT₃ : Measurable T₃)
+
+/-- For any $T_1, T_2, T_3$, $k$ is at most
+$$ \delta + \eta (d[X^0_1;T_1|T_3]-d[X^0_1;X_1]) + \eta (d[X^0_2;T_2|T_3]-d[X^0_2;X_2])$$
+where $\delta = I[T₁ : T₂ ; μ] + I[T₂ : T₃ ; μ] + I[T₃ : T₁ ; μ]$. -/
+proof_wanted construct_good_prelim' : 0 = 1
+
+/-- In fact $k$ is at most
+ $$ \delta + \frac{\eta}{6}  \sum_{i=1}^2 \sum_{1 \leq j,l \leq 3; j \neq l} (d[X^0_i;T_j|T_l] - d[X^0_i; X_i]).$$
+-/
+proof_wanted construct_good' : 0 = 1
+
+/--   $k$ is at most
+$$ \leq I(U : V \, | \, S) + I(V : W \, | \,S) + I(W : U \, | \, S) + \frac{\eta}{6}  \sum_{i=1}^2 \sum_{A,B \in \{U,V,W\}: A \neq B} (d[X^0_i;A|B,S] - d[X^0_i; X_i]).$$
+-/
+proof_wanted averaged_construct_good : 0 = 1
+
+
+/-- Let $X_1, X_2, X_3, X_4$ be independent $G$-valued random variables, and let $Y$ be another $G$-valued random variable.  Set $S := X_1+X_2+X_3+X_4$. Then $d[Y; X_1+X_2|X_1 + X_3, S] - d[Y; X_1]$ is at most
+$$ \tfrac{1}{4} (2d[X_1;X_2] + d[X_1;X_3] + d[X_3;X_4])$$
+$$+ \tfrac{1}{8} (\bbH[X_1+X_2] - \bbH[X_1+X_3] + \bbH[X_2] - \bbH[X_3]$$
+$$ + \bbH[X_2|X_2+X_4] - \bbH[X_1|X_1+X_3]).$$
+-/
+proof_wanted gen_ineq: 0 = 1
+
+/-- The quantity
+$$ \sum_{i=1}^2 \sum_{A,B \in \{U,V,W\}: A \neq B}  d[X_i^0;A|B, S] - d[X_i^0;X_i]$$
+is at most $12 k + \frac{4(2 \eta k - I_1)}{1-\eta}$.
+-/
+proof_wanted dist_diff_bound : 0 = 1
+
+/-- Suppose $0 < \eta < 1/8$.  Let $X_1, X_2$ be tau-minimizers.  Then $d[X_1;X_2] = 0$.
+-/
+theorem tau_strictly_decreases' : d[X₁ # X₂] = 0 := sorry
+
+end MainEstimates
+
+section EntropicPFR
+
+open MeasureTheory ProbabilityTheory
+universe uG
+
+variable {Ω₀₁ Ω₀₂ : Type*} [MeasureSpace Ω₀₁] [MeasureSpace Ω₀₂]
+
+variable {Ω Ω' : Type*} [mΩ : MeasureSpace Ω] [IsProbabilityMeasure (ℙ : Measure Ω)]
+  [IsProbabilityMeasure (ℙ : Measure Ω₀₁)] [IsProbabilityMeasure (ℙ : Measure Ω₀₂)]
+
+variable {G : Type uG} [AddCommGroup G] [ElementaryAddCommGroup G 2] [Fintype G] [MeasurableSpace G]
+  [MeasurableSingletonClass G]
+variable (p : refPackage Ω₀₁ Ω₀₂ G) {X₁ : Ω → G} {X₂ : Ω → G} (hX₁ : Measurable X₁) (hX₂ : Measurable X₂)
+
+/-- `entropic_PFR_conjecture'`: For two $G$-valued random variables $X^0_1, X^0_2$, there is some
+    subgroup $H \leq G$ such that $d[X^0_1;U_H] + d[X^0_2;U_H] \le (2+1/η) d[X^0_1;X^0_2]$. -/
+theorem entropic_PFR_conjecture' (hpη: p.η = 1/9):
+    ∃ H : AddSubgroup G, ∃ Ω : Type uG, ∃ mΩ : MeasureSpace Ω, ∃ U : Ω → G,
+    IsProbabilityMeasure (ℙ : Measure Ω) ∧ Measurable U ∧
+    IsUniform H U ∧ d[p.X₀₁ # U] + d[p.X₀₂ # U] ≤ (2+1/p.η) * d[p.X₀₁ # p.X₀₂] := sorry
+
+end EntropicPFR
+
+
+section PFR
+
+open Pointwise Set MeasureTheory ProbabilityTheory
+
+variable {G Ω : Type*} [AddCommGroup G] [Fintype G]
+    [MeasurableSpace G] [MeasurableSingletonClass G] {A B : Set G}
+    [MeasureSpace Ω] [IsProbabilityMeasure (ℙ : Measure Ω)] {U V : Ω → G}
+
+/-- The polynomial Freiman-Ruzsa (PFR) conjecture: if $A$ is a subset of an elementary abelian
+2-group of doubling constant at most $K$, then $A$ can be covered by at most $2K^{11}$ cosets of
+a subgroup of cardinality at most $|A|$. -/
+theorem PFR_conjecture' (h₀A : A.Nonempty) (hA : Nat.card (A + A) ≤ K * Nat.card A) :
+     ∃ (H : AddSubgroup G) (c : Set G),
+      Nat.card c < 2 * K ^ 11 ∧ Nat.card H ≤ Nat.card A ∧ A ⊆ c + H := by
+  sorry
+
+end PFR
