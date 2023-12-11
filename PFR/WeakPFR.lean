de285998d02b649d6a742c486792d16c73e10ac7
teorth <tao@math.ucla.edu>
2023-12-11 08:15:29 -0800

    Add weakPFR file, leans

--- PFR/WeakPFR.lean
+++ PFR/WeakPFR.lean
@@ -7,8 +7,50 @@ Here we use the entropic form of PFR to deduce a weak form of PFR over the integ
 
 ## Main statement
 
-* `weak_PFR_int`: 
+* `weak_PFR_int`: Let $A\subseteq \mathbb{Z}^d$ and $\lvert A+A\rvert\leq K\lvert A\rvert$. There exists $A'\subseteq A$ such that $\lvert A'\rvert \geq K^{-48}\lvert A\rvert$ and $\dim A' \leq 60\log K$.
 
 -/
 
-open MeasureTheory ProbabilityTheory
+
+/-- If $G$ is torsion-free and $X,Y$ are $G$-valued random variables then $d[X;2Y]\leq 5d[X;Y]$.  -/
+proof_wanted torsion_free_doubling : 0 = 1
+
+/-- If $G$ is a torsion-free group and $X,Y$ are $G$-valued random variables and $\phi:G\to \mathbb{F}_2^d$ is a homomorphism then
+\[\mathbb{H}(\phi(X))\leq 10d[X;Y].\] -/
+proof_wanted torsion_dist_shrinking : 0 = 1
+
+/-- Let $G=\mathbb{F}_2^n$ and $X,Y$ be $G$-valued random variables such that
+\[\mathbb{H}(X)+\mathbb{H}(Y)> 48d[X;Y].\]
+There is a non-trivial subgroup $H\leq G$ such that
+\[\log \lvert H\rvert <\mathbb{H}(X)+\mathbb{H}(Y)\] and
+\[\mathbb{H}(\psi(X))+\mathbb{H}(\psi(Y))< \frac{\mathbb{H}(X)+\mathbb{H}(Y)}{2}\]
+where $\psi:G\to G/H$ is the natural projection homomorphism.
+-/
+proof_wanted app_ent_PFR : 0 = 1
+
+/-- If $G=\mathbb{F}_2^d$ and $X,Y$ are $G$-valued random variables then there is a subgroup $H\leq \mathbb{F}_2^d$ such that
+\[\log \lvert H\rvert \leq 2(\mathbb{H}(X)+\mathbb{H}(Y))\]
+and if $\psi:G \to G/H$ is the natural projection then
+\[\mathbb{H}(\psi(X))+\mathbb{H}(\psi(Y))\leq 48 d[\psi(X);\psi(Y)].\] -/
+proof_wanted PFR_projection : 0 = 1
+
+/-- Let $\phi:G\to H$ be a homomorphism and $A,B\subseteq G$ be finite subsets. If $x,y\in H$ then let $A_x=A\cap \phi^{-1}(x)$ and $B_y=B\cap \phi^{-1}(y)$. There exist $x,y\in H$ such that $A_x,B_y$ are both non-empty and
+\[d[\phi(U_A);\phi(U_B)]\log \frac{\lvert A\rvert\lvert B\rvert}{\lvert A_x\rvert\lvert B_y\rvert}\leq (\mathbb{H}(\phi(U_A))+\mathbb{H}(\phi(U_B)))(d(U_A,U_B)-d(U_{A_x},U_{B_y}).\] -/
+proof_wanted single_fibres : 0 = 1
+
+
+/-- If $A\subseteq \mathbb{Z}^{d}$ then by $\dim(A)$ we mean the dimension of the span of $A-A$ over the reals -- equivalently, the smallest $d'$ such that $A$ lies in a coset of a subgroup isomorphic to $\mathbb{Z}^{d'}$. -/
+def dimension := 0
+
+/-- If $A,B\subseteq \mathbb{Z}^d$ are finite non-empty sets then there exist non-empty $A'\subseteq A$ and $B'\subseteq B$ such that
+\[\log\frac{\lvert A\rvert\lvert B\rvert}{\lvert A'\rvert\lvert B'\rvert}\leq 48d[U_A;U_B]\]
+such that $\max(\dim A',\dim B')\leq \frac{40}{\log 2} d[U_A;U_B]$. -/
+proof_wanted weak_PFR_asymm : 0 = 1
+
+/-- If $A\subseteq \mathbb{Z}^d$ is a finite non-empty set with $d[U_A;U_A]\leq \log K$ then there exists a non-empty $A'\subseteq A$ such that
+\[\lvert A'\rvert\geq K^{-48}\lvert A\rvert\]
+and $\dim A'\leq 60\log K$. -/
+proof_wanted weak_PFR : 0 = 1
+
+/-- Let $A\subseteq \mathbb{Z}^d$ and $\lvert A+A\rvert\leq K\lvert A\rvert$. There exists $A'\subseteq A$ such that $\lvert A'\rvert \geq K^{-48}\lvert A\rvert$ and $\dim A' \leq 60\log K$.-/
+proof_wanted weak_PFR_int : 0 = 1
