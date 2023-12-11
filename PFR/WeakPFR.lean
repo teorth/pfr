25309adace9c0181538822127f443915d0a12e2d
teorth <tao@math.ucla.edu>
2023-12-11 08:06:33 -0800

    Starting WeakPFR file

---
+++ PFR/WeakPFR.lean
@@ -0,0 +1,14 @@
+import PFR.EntropyPFR
+
+/-!
+# Weak PFR over the integers
+
+Here we use the entropic form of PFR to deduce a weak form of PFR over the integers.
+
+## Main statement
+
+* `weak_PFR_int`: 
+
+-/
+
+open MeasureTheory ProbabilityTheory
