6a8de6d4a66e08f5ee95299684c425d78fa9841c
L Lllvvuu <git@llllvvuu.dev>
2024-01-10 03:00:21 -0800

    feat: finished `ent_of_proj_le`

--- PFR/ForMathlib/Entropy/Basic.lean
+++ PFR/ForMathlib/Entropy/Basic.lean
@@ -620,7 +620,6 @@ lemma entropy_comp_le (μ : Measure Ω) [IsProbabilityMeasure μ] (hX : Measurab
   simp only [le_add_iff_nonneg_right]
   exact condEntropy_nonneg X (f ∘ X) μ
 
-
 /-- A Schroder-Bernstein type theorem for entropy : if two random variables are functions of each
   other, then they have the same entropy. Can be used as a substitute for
   `entropy_comp_of_injective` if one doesn't want to establish the injectivity. -/
@@ -912,16 +911,16 @@ $$ H[Y|f(X)] \geq H[Y|X]$$
 To upgrade this to equality, see `condEntropy_of_injective` -/
 lemma condEntropy_comp_ge [FiniteRange X] [FiniteRange Y] (μ : Measure Ω) [IsProbabilityMeasure μ]
     (hX : Measurable X) (hY : Measurable Y) (f : S → U) : H[Y | f ∘ X ; μ] ≥ H[Y | X; μ] := by
-  rewrite [chain_rule'' _ hY hX, ← entropy_prod_comp hX μ f]
-  have hZ : Measurable (f ∘ X) := (measurable_of_countable _).comp hX
-  have : H[⟨Y, ⟨X, f ∘ X⟩⟩ ; μ] = H[⟨Y, X⟩ ; μ] := by
+  have h_joint : H[⟨Y, ⟨X, f ∘ X⟩⟩ ; μ] = H[⟨Y, X⟩ ; μ] := by
     let g : T × S → T × S × U := fun (y, x) ↦ (y, (x, f x))
     show H[g ∘ ⟨Y, X⟩ ; μ] = H[⟨Y, X⟩ ; μ]
     refine entropy_comp_of_injective μ (by exact Measurable.prod hY hX) g (fun _ _ h => ?_)
     repeat rewrite [Prod.mk.injEq] at h
     exact Prod.ext h.1 h.2.1
-  rewrite [← this, ← chain_rule'' _ hY (Measurable.prod (by exact hX) (by exact hZ))]
-  exact entropy_submodular _ hY hX hZ
+  have hZ : Measurable (f ∘ X) := (measurable_of_countable _).comp hX
+  rewrite [chain_rule'' μ hY hX, ← entropy_prod_comp hX μ f, ← h_joint,
+    ← chain_rule'' μ hY (Measurable.prod (by exact hX) (by exact hZ))]
+  exact entropy_submodular μ hY hX hZ
 
 /-- The submodularity inequality:
 $$ H[X, Y, Z] + H[Z] \leq H[X, Z] + H[Y, Z].$$ -/
