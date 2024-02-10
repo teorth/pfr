d26b51d76d4351587278f2847e81a422210b7d4f
L Lllvvuu <git@llllvvuu.dev>
2024-02-09 22:24:02 -0800

    golf

--- PFR/ApproxHomPFR.lean
+++ PFR/ApproxHomPFR.lean
@@ -103,8 +103,6 @@ theorem approx_hom_pfr (f : G → G') (K : ℝ) (hK: K > 0)
 
   let A'' := A'.toSet
   have hA''_coe : Nat.card A'' = Finset.card A' := Nat.card_eq_finsetCard A'
-  have h_pos1 : 0 < (C₁ : ℝ) * K ^ 2 := mul_pos (by unfold C₁; norm_num) (pow_pos hK 2)
-  have hA''₀ : A''.Nonempty := hA'₀.to_set
   have hA''_pos : 0 < Nat.card A'' := by rw [hA''_coe]; exact hA'₀.card_pos
   have hA''_nonempty : Set.Nonempty A'' := nonempty_subtype.mp (Finite.card_pos_iff.mp hA''_pos)
   have : Finset.card (A' - A') = Nat.card (A'' + A'') := calc
@@ -117,8 +115,7 @@ theorem approx_hom_pfr (f : G → G') (K : ℝ) (hK: K > 0)
       _ = 2 ^ 14 * K ^ 12 * ((2 ^ 4)⁻¹ * (K ^ 2)⁻¹ * A.card) := by
         rw [mul_mul_mul_comm, ← mul_assoc (2^14), ← Real.rpow_two, mul_assoc _ (_)⁻¹,
           ← mul_assoc (_)⁻¹, ← Real.rpow_neg (by positivity),
-          show K ^ (12 : ℕ) = K ^ (12 : ℝ) from (Real.rpow_nat_cast K 12).symm,
-          show (K ^ (2 : ℝ)) ^ (5 : ℕ) = (K ^ (2 : ℝ)) ^ (5 : ℝ) from (Real.rpow_nat_cast _ 5).symm,
+          (Real.rpow_nat_cast K 12).symm, (Real.rpow_nat_cast _ 5).symm,
           ← Real.rpow_add (by positivity), ← Real.rpow_mul (by positivity), mul_assoc]
         norm_num
       _ ≤ _ := by gcongr
