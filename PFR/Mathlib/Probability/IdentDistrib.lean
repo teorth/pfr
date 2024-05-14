52095a258c1e2a8f745a2644bc70bd196396b600
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-05-14 23:50:32 +0200

    Cleanup
    Co-authored-by: Lorenzo Luccioli <LorenzoLuccioli@users.noreply.github.com>

--- PFR/Mathlib/Probability/IdentDistrib.lean
+++ PFR/Mathlib/Probability/IdentDistrib.lean
@@ -175,7 +175,7 @@ lemma identDistrib_comp_fst {X : Ω → α} (hX : Measurable X) (μ : Measure Ω
 
 /-- A random variable is identically distributed to its lift to a product space (in the second factor). -/
 lemma identDistrib_comp_snd {X : Ω → α} (hX : Measurable X) (μ : Measure Ω) (μ' : Measure Ω')
-  [SigmaFinite μ][IsProbabilityMeasure μ'] : IdentDistrib (X ∘ Prod.snd) X (μ'.prod μ) μ where
+  [SigmaFinite μ] [IsProbabilityMeasure μ'] : IdentDistrib (X ∘ Prod.snd) X (μ'.prod μ) μ where
   aemeasurable_fst := (hX.comp measurable_snd).aemeasurable
   aemeasurable_snd := hX.aemeasurable
   map_eq := by
