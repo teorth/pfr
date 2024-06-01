7f94a22192094908641cce625dd2c236e1988397
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-06-01 19:35:34 +0200

    add `iIndepFun.iIndepFun_finsets` and `iIndepFun.iIndepFun_finsets_comp`
    Co-authored-by: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>

--- PFR/Mathlib/Probability/Independence/Basic.lean
+++ PFR/Mathlib/Probability/Independence/Basic.lean
@@ -63,6 +63,29 @@ lemma iIndepFun.inv (h : iIndepFun n f μ) : iIndepFun n (update f i (f i)⁻¹)
   · subst hj; simp [measurable_inv]
   · simp [hj, measurable_id]
 
+/-- If `f` is a family of mutually independent random variables, `(S j)ⱼ` are pairwise disjoint
+finite index sets, then the tuples formed by `f i` for `i ∈ S j` are mutually independent,
+when seen as a family indexed by `J`. -/
+lemma iIndepFun.iIndepFun_finsets {f : ∀ i, Ω → β i} {J : Type*} [Fintype J]
+    (S : J → Finset ι) (h_disjoint : Set.PairwiseDisjoint Set.univ S)
+    (hf_Indep : iIndepFun m f μ) (hf_meas : ∀ i, Measurable (f i)) :
+    iIndepFun (fun _ ↦ pi) (fun (j : J) ↦ fun a (i : S j) ↦ f i a) μ := by
+  have := hf_Indep.isProbabilityMeasure
+  exact kernel.iIndepFun.iIndepFun_finsets S h_disjoint hf_Indep hf_meas
+
+/-- If `f` is a family of mutually independent random variables, `(S j)ⱼ` are pairwise disjoint
+finite index sets, and `φ j` is a function that maps the tuple formed by `f i` for `i ∈ S j` to a
+measurable space `γ j`, then the family of random variables formed by `φ j (f i)_{i ∈ S j}` and
+indexed by `J` is iIndep. -/
+lemma iIndepFun.iIndepFun_finsets_comp {f : ∀ i, Ω → β i} {J : Type*} [Fintype J]
+    (S : J → Finset ι) (h_disjoint : Set.PairwiseDisjoint Set.univ S)
+    (hf_Indep : iIndepFun m f μ) (hf_meas : ∀ i, Measurable (f i))
+    (γ : J → Type*) {mγ : ∀ j, MeasurableSpace (γ j)}
+    (φ : (j : J) → ((i : S j) → β i) → γ j) (hφ : ∀ j, Measurable (φ j)) :
+    iIndepFun mγ (fun (j : J) ↦ fun a ↦ φ j (fun (i : S j) ↦ f i a)) μ :=by
+  have := hf_Indep.isProbabilityMeasure
+  exact kernel.iIndepFun.iIndepFun_finsets_comp S h_disjoint hf_Indep hf_meas γ φ hφ
+
 end iIndepFun
 
 section
