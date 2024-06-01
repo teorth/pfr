f6b90cea43cc9b344b45fbec88511d3f6dfe98a2
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-06-01 19:41:06 +0200

    refactor names

--- PFR/Mathlib/Probability/Independence/Kernel.lean
+++ PFR/Mathlib/Probability/Independence/Kernel.lean
@@ -47,7 +47,7 @@ lemma iIndepFun.comp (h : iIndepFun m f κ μ) (g : ∀ i, β i → γ i) (hg : 
 /-- If `f` is a family of mutually independent random variables, `(S j)ⱼ` are pairwise disjoint
 finite index sets, then the tuples formed by `f i` for `i ∈ S j` are mutually independent,
 when seen as a family indexed by `J`. -/
-lemma iIndepFun.iIndepFun_finsets [IsMarkovKernel κ] {J : Type*} [Fintype J]
+lemma iIndepFun.finsets [IsMarkovKernel κ] {J : Type*} [Fintype J]
     (S : J → Finset ι) (h_disjoint : Set.PairwiseDisjoint Set.univ S)
     (hf_Indep : iIndepFun m f κ μ) (hf_meas : ∀ i, Measurable (f i)) :
     iIndepFun (fun _ ↦ pi) (fun (j : J) ↦ fun a (i : S j) ↦ f i a) κ μ := by
@@ -58,12 +58,12 @@ lemma iIndepFun.iIndepFun_finsets [IsMarkovKernel κ] {J : Type*} [Fintype J]
 finite index sets, and `φ j` is a function that maps the tuple formed by `f i` for `i ∈ S j` to a
 measurable space `γ j`, then the family of random variables formed by `φ j (f i)_{i ∈ S j}` and
 indexed by `J` is iIndep. -/
-lemma iIndepFun.iIndepFun_finsets_comp [IsMarkovKernel κ] {J : Type*} [Fintype J]
+lemma iIndepFun.finsets_comp [IsMarkovKernel κ] {J : Type*} [Fintype J]
     (S : J → Finset ι) (h_disjoint : Set.PairwiseDisjoint Set.univ S)
     (hf_Indep : iIndepFun m f κ μ) (hf_meas : ∀ i, Measurable (f i))
     (γ : J → Type*) {mγ : ∀ j, MeasurableSpace (γ j)}
     (φ : (j : J) → ((i : S j) → β i) → γ j) (hφ : ∀ j, Measurable (φ j)) :
     iIndepFun mγ (fun (j : J) ↦ fun a ↦ φ j (fun (i : S j) ↦ f i a)) κ μ :=
-  (kernel.iIndepFun.iIndepFun_finsets S h_disjoint hf_Indep hf_meas).comp φ hφ
+  (kernel.iIndepFun.finsets S h_disjoint hf_Indep hf_meas).comp φ hφ
 
 end iIndepFun
