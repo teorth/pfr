036eec219ce1efc1eb2efb32219df4ccfcc5f8ec
Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>
2025-04-15 23:19:44 +0200

    one more

--- PFR/Mathlib/Probability/ConditionalProbability.lean
+++ PFR/Mathlib/Probability/ConditionalProbability.lean
@@ -13,4 +13,15 @@ lemma cond_real_apply (hms : MeasurableSet s) (μ : Measure Ω) (t : Set Ω) :
     μ[|s].real t = (μ.real s)⁻¹ * μ.real (s ∩ t) := by
   simp [Measure.real, cond_apply hms]
 
+/-- The conditional probability measure of any finite measure on any set of positive measure
+is a probability measure. -/
+theorem cond_isProbabilityMeasure_of_real {α : Type*} {_ : MeasurableSpace α} {μ : Measure α}
+    {s : Set α} (hcs : μ.real s ≠ 0) :
+    IsProbabilityMeasure μ[|s] := by
+  apply cond_isProbabilityMeasure_of_finite
+  · intro h
+    simp [measureReal_def, h] at hcs
+  · intro h
+    simp [measureReal_def, h] at hcs
+
 end ProbabilityTheory
